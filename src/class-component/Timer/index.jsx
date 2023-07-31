c86f0471ed73c586adb5a9a0e194a1a5000ae8f0
Vishal Sharma <785vishal443@gmail.com>
2023-07-31 22:12:59 +0530

    react life cycle

--- src/class-component/Timer/index.jsx
+++ src/class-component/Timer/index.jsx
@@ -6,6 +6,8 @@ class Timer extends Component {
         // all the initialization things you can do
         this.state = {
             timer: 0,
+            name:'Watch',
+            lastName: 'Chimachom'
         }
     }
     componentWillMount() {
@@ -24,16 +26,41 @@ class Timer extends Component {
         // useEffect(()=>{
 
         // } , [])
+        // here you call any api or any subscribed
     }
 
-    shouldComponentUpdate () {
+    // if this is child component parent  state  => child props
+    shouldComponentUpdate (nextProps , nextState) {
+        console.log(nextState , this.state)
+        // if(2*this.state.timer === nextState.timer) {
+        //     return true
+        // }
         // this is called before updating state
         // perormance
+        // if this will reture return than only component re-render happen
+        // it will call just before component updated
       return true;
     }
 
-    componentDidUpdate(){     
+    callApi () {
+        alert('calling api....')
+    }
+
+    componentDidUpdate(nextProps , nextState){  
+        // USE OF THIS METHOD IS GENERALLY CALLING SIDE EFFECT ON UPDATING ANY STATE
+        // API CALL   
+        
          console.log('did update')
+         if(nextState.timer+1 === this.state.timer) {
+            this.callApi()
+        }
+
+        //  useEffect(()=>{
+
+        //  } ,[timer])
+        
+         // when state updated immediately it will call
+         // this will call once component updated and re-render happen
     }
 
     componentWillUnmount() {
@@ -41,10 +68,13 @@ class Timer extends Component {
     }
 
     updateTimer() {
- // update satte  
- this.setState({timer:  1234})
+     this.setState({timer:  this.state.timer+1} , ()=> {
+    console.log('callback' ,this.state.name)
+     })
+     console.log('outside' ,this.state.timer)
     }
     render () {
+        console.log(this.state.lastName)
             return  (
                 <>
                 <div>This First  React Class Component {this.state.timer}</div>
